2c96f37654cbb3c526465d7736668974c827fa01
Tony Lu <konigderspiele@live.com>
2023-05-11 12:44:20 -0500

    timeout version

--- user/primes.c
+++ user/primes.c
@@ -3,8 +3,7 @@
 
 void primes(int* buf) {
   int first = 1, firstPrime = 1;
-  int prime, num;
-  int p[2];
+  int prime, num, p[2];
   while (read(buf[0], &num, 4) > 0) {
     if (first) {
       prime = num;
@@ -13,20 +12,19 @@ void primes(int* buf) {
     } else if (num % prime != 0) {
       if (firstPrime) {
         pipe(p);
-      }
-      write(p[1], &num, 4);
-      if (firstPrime) {
-	if (fork() == 0) {
-	  firstPrime = 0;
+        firstPrime = 0;
+        if (fork() == 0) {
           primes(p);
         }
       }
+      write(p[1], &num, 4);
     }
   }
   close(buf[0]);
   close(buf[1]);
   close(p[0]);
   close(p[1]);
+  wait(0);
   exit(0);
 }
 
@@ -45,6 +43,7 @@ main(int argc, char *argv[])
   }
   close(p[0]);
   close(p[1]);
+  wait(0);
 
   exit(0);
 }
