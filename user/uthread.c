505ba3c7b7d9ebdee556cba25ee561238f34b945
Tony Lu <konigderspiele@live.com>
2023-06-19 16:51:51 -0500

    working uthread tests

--- user/uthread.c
+++ user/uthread.c
@@ -14,6 +14,19 @@
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+  uint64     sp;
+  uint64     s0;
+  uint64     s1;
+  uint64     s2;
+  uint64     s3;
+  uint64     s4;
+  uint64     s5;
+  uint64     s6;
+  uint64     s7;
+  uint64     s8;
+  uint64     s9;
+  uint64     s10;
+  uint64     s11;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -62,6 +75,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64)t, (uint64)next_thread);
   } else
     next_thread = 0;
 }
@@ -75,7 +89,8 @@ thread_create(void (*func)())
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
-  // YOUR CODE HERE
+  t->sp = (uint64)t->stack + STACK_SIZE;
+  *((void**)t->sp - 1) = func;
 }
 
 void 
